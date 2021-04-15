a88752fdb8590d1b66dfbf5fe03ddc176c430de1
Jonas Nascimento <jonas.cba@gmail.com>
2021-04-15 05:36:10 -0400

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -31,4 +31,21 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		/**
+		 * getOne does the User instance, but does not go to the database,
+		 * just monitor the object JPA for further interaction with the 
+		 * database, getOne is better than findById.
+		 */
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
